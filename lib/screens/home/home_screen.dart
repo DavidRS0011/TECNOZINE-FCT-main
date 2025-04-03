757b0c2817ab7b10d39c3543d6d68d322318ad67
DavidRS0011 <drs0011@alu.medac.es>
2025-04-03 11:41:16 +0200

    organizacion del directorio

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import '../widgets/product_dialog.dart';
+import '../../widgets/product_dialog.dart';
 import '../services/database_service.dart';
 
 class HomeScreen extends StatefulWidget {
--- lib/screens/login/widgets/login_screen.dart
+++ lib/screens/login/widgets/login_screen.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
-import 'package:flutter_prueba_mil/screens/widgets/animated_logo.dart';
+import 'package:flutter_prueba_mil/widgets/animated_logo.dart';
 import 'package:flutter_prueba_mil/screens/home/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
